bc55283dd108305dabdd1396cc40451757ae148c
WhyAre <laiyr52@gmail.com>
2025-03-16 23:39:48 +0800

    Add error message if 'tutorial' is typed without any sub commands

--- src/main/java/seedu/address/logic/parser/TutorialParser.java
+++ src/main/java/seedu/address/logic/parser/TutorialParser.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.AddTutorialCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteTutorialCommand;
@@ -17,18 +18,32 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class TutorialParser implements Parser<Command> {
 
     private final Map<String, Parser<? extends Command>> subcmds;
+    private final String USAGE;
 
     TutorialParser() {
         subcmds = new HashMap<>();
         subcmds.put(AddTutorialCommand.COMMAND_WORD, new AddTutorialCommandParser());
         subcmds.put(ListTutorialCommand.COMMAND_WORD, new ListTutorialCommandParser());
         subcmds.put(DeleteTutorialCommand.COMMAND_WORD, new DeleteTutorialCommandParser());
+
+        USAGE = """
+                        Usage: tutorial COMMAND
+
+                        COMMAND:
+                        %s""".formatted(
+                        subcmds.keySet().stream().map("  - %s"::formatted).collect(Collectors.joining("\n")));
     }
 
     @Override
     public Command parse(String arguments) throws ParseException {
+        // Arguments does not have the word "tutorial" in it
+
         var cmd = arguments.trim().split(" ");
         var rest = Arrays.stream(cmd).skip(1).collect(Collectors.joining(" "));
+
+        if (!subcmds.containsKey(cmd[0])) {
+            throw new ParseException(Messages.MESSAGE_INVALID_COMMAND_FORMAT.formatted(USAGE));
+        }
 
         return subcmds.get(cmd[0]).parse(rest);
     }
