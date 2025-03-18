313f67aeb187e79e9243f7a10ab5a61b34f24aab
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-03-18 23:09:54 +0800

    Fix formatting

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -41,9 +41,10 @@ public class EditCommand extends Command {
                     + "by the index number used in the displayed student list. "
                     + "Existing values will be overwritten by the input values.\n"
                     + "Parameters: INDEX (must be a positive integer) " + "[" + PREFIX_NAME + "NAME] " + "["
-                    + PREFIX_ID_STUDENT + "STUDENT_ID] " + PREFIX_PHONE + "PHONE] " + "[" + PREFIX_EMAIL + "EMAIL] "
-                    + "[" + PREFIX_HANDLE + "HANDLE] " + "[" + PREFIX_TUTORIAL_NAME + "TUTORIALS]...\n" + "Example: "
-                    + COMMAND_WORD + " 1 " + PREFIX_PHONE + "91234567 " + PREFIX_EMAIL + "johndoe@example.com";
+                    + PREFIX_ID_STUDENT + "STUDENT_ID] " + "[" + PREFIX_PHONE + "PHONE] " + "[" + PREFIX_EMAIL
+                    + "EMAIL] " + "[" + PREFIX_HANDLE + "HANDLE] " + "[" + PREFIX_TUTORIAL_NAME + "TUTORIALS]...\n"
+                    + "Example: " + COMMAND_WORD + " 1 " + PREFIX_PHONE + "91234567 " + PREFIX_EMAIL
+                    + "johndoe@example.com";
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Student: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
