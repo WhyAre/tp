d885059d0bdf19411a7a3738a900fdfd054d4650
WhyAre <laiyr52@gmail.com>
2025-03-20 00:34:40 +0800

    Fix broken JSON files

--- src/test/java/seedu/address/testutil/TypicalStudents.java
+++ src/test/java/seedu/address/testutil/TypicalStudents.java
@@ -95,6 +95,6 @@ public class TypicalStudents {
     }
 
     public static List<Tutorial> getTypicalTutorials() {
-        return List.of(new Tutorial("CS2103-T1"), new Tutorial("CS2106-T37"));
+        return List.of(new Tutorial("CS2103-T1"), new Tutorial("CS2106-T02"), new Tutorial("CS2106-T37"));
     }
 }
