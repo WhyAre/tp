862f1731f87ffaea95d7dbd97e6c154c5c61571e
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-03-19 13:42:02 +0800

    Modify edit command to proceed on unadded tutorial
    Minor fix to #105
    Edit command now follows add command in behaviour.
    All other edits made to student will proceed, with only existing tutorials (if any).

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -85,15 +85,20 @@ public class EditCommand extends Command {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
+        String resultMessage = MESSAGE_EDIT_PERSON_SUCCESS;
+        final Set<Tutorial> existingTutorials = new HashSet<>();
         for (Tutorial tutorial : editedStudent.getTutorials()) {
             if (!model.hasTutorial(tutorial)) {
-                throw new CommandException(MESSAGE_TUTORIAL_NOT_FOUND + tutorial.name());
+                resultMessage += "\n" + MESSAGE_TUTORIAL_NOT_FOUND + tutorial.name();
+            } else {
+                existingTutorials.add(tutorial);
             }
         }
+        editedStudent.setTutorials(existingTutorials);
 
         model.setStudent(studentToEdit, editedStudent);
         model.updateFilteredStudentList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedStudent)));
+        return new CommandResult(String.format(resultMessage, Messages.format(editedStudent)));
     }
 
     /**
