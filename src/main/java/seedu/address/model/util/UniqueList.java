ddbbc16fe691d261c9ff6249467164e1b1416171
WhyAre <laiyr52@gmail.com>
2025-03-23 12:48:28 +0800

    Add UniqueList

---
+++ src/main/java/seedu/address/model/util/UniqueList.java
@@ -0,0 +1,239 @@
+package seedu.address.model.util;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.IntStream;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Wrapper around a list of {@link Identifiable} List cannot contain duplicate,
+ * or null.
+ * <p>
+ * Duplicate detection checked done by {@link Identifiable#hasSameIdentity}
+ */
+public class UniqueList<T extends Identifiable<T>> implements List<T> {
+
+    private final ObservableList<T> internalList = FXCollections.observableArrayList();
+    private final ObservableList<T> internalUnmodifiableList = FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if {@code items} contains only unique students.
+     */
+    private boolean areItemsUnique(List<T> items) {
+        return IntStream.range(0, items.size()).allMatch(i -> IntStream.range(i + 1, items.size())
+                        .noneMatch(j -> items.get(i).hasSameIdentity(items.get(j))));
+    }
+
+    /**
+     * Checks whether the list contains the object based on
+     * {@link Identifiable#hasSameIdentity}
+     */
+    public boolean isUnique(T toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().noneMatch(toCheck::hasSameIdentity);
+    }
+
+    public void setAll(UniqueList<T> replacement) {
+        setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code students}. {@code students}
+     * must not contain duplicate students.
+     */
+    public void setAll(List<T> items) {
+        requireAllNonNull(items);
+        if (!areItemsUnique(items)) {
+            throw new IllegalStateException("Students list contains duplicate students");
+        }
+
+        internalList.setAll(items);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<T> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    /**
+     * Removes object from the list
+     */
+    public boolean remove(T toRemove) {
+        return internalList.remove(toRemove);
+    }
+
+    @Override
+    public T remove(int i) {
+        return internalList.remove(i);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return internalList.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return internalList.removeAll(collection);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return internalList.retainAll(collection);
+    }
+
+    @Override
+    public void clear() {
+        internalList.clear();
+    }
+
+    @Override
+    public int size() {
+        return internalList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return internalList.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return internalList.contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return internalList.toArray();
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return internalList.toArray(t1s);
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (otherObject == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(otherObject instanceof UniqueList<?> other)) {
+            return false;
+        }
+
+        return internalList.equals(other.internalList);
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    @Override
+    public T get(int i) {
+        return internalList.get(i);
+    }
+
+    /**
+     * Replaces the old with new {@code target} must exist in the list. The student
+     * identity of {@code editedStudent} must not be the same as another existing
+     * student in the list.
+     */
+    public void set(T oldItem, T newItem) {
+        requireAllNonNull(oldItem, newItem);
+
+        int index = internalList.indexOf(oldItem);
+        if (index == -1) {
+            throw new IllegalStateException("Item not exists");
+        }
+
+        if (!oldItem.hasSameIdentity(newItem) && !isUnique(newItem)) {
+            throw new IllegalStateException("Item already exists");
+        }
+
+        internalList.set(index, newItem);
+    }
+
+    @Override
+    public T set(int i, T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Adds an item into the list; Item must not already exist in the list
+     */
+    @Override
+    public boolean add(T toAdd) {
+        requireNonNull(toAdd);
+        if (!isUnique(toAdd)) {
+            return false;
+        }
+        return internalList.add(toAdd);
+    }
+
+    @Override
+    public void add(int i, T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return internalList.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return internalList.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return internalList.listIterator();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return internalList.listIterator(i);
+    }
+
+    @Override
+    public List<T> subList(int i, int i1) {
+        return internalList.subList(i, i1);
+    }
+
+    @Override
+    public String toString() {
+        return internalList.toString();
+    }
+}
---
+++ src/test/java/seedu/address/model/util/UniqueListTest.java
@@ -0,0 +1,183 @@
+package seedu.address.model.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_HANDLE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TUTORIAL_2;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalStudents.ALICE;
+import static seedu.address.testutil.TypicalStudents.BOB;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.student.Student;
+import seedu.address.testutil.StudentBuilder;
+
+public class UniqueListTest {
+
+    private final UniqueList<Student> uniqueStudentList = new UniqueList<>();
+
+    @Test
+    public void isUnique_nullStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.isUnique(null));
+    }
+
+    @Test
+    public void isUnique_studentNotInList_returnsFalse() {
+        assertTrue(uniqueStudentList.isUnique(ALICE));
+    }
+
+    @Test
+    public void isUnique_studentInList_returnsTrue() {
+        uniqueStudentList.add(ALICE);
+        assertFalse(uniqueStudentList.isUnique(ALICE));
+    }
+
+    @Test
+    public void isUnique_studentWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueStudentList.add(ALICE);
+        Student editedAlice = new StudentBuilder(ALICE).withHandle(VALID_HANDLE_BOB).withTutorials(VALID_TUTORIAL_2)
+                        .build();
+        assertFalse(uniqueStudentList.isUnique(editedAlice));
+    }
+
+    @Test
+    public void add_nullStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.add(null));
+    }
+
+    @Test
+    public void add_duplicateStudent_throwsDuplicateStudentException() {
+        uniqueStudentList.add(ALICE);
+        assertFalse(uniqueStudentList.add(ALICE));
+    }
+
+    @Test
+    public void setStudent_nullTargetStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.set(null, ALICE));
+    }
+
+    @Test
+    public void setStudent_nullEditedStudent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.set(ALICE, null));
+    }
+
+    @Test
+    public void setStudent_targetStudentNotInList_throwsStudentNotFoundException() {
+        assertThrows(IllegalStateException.class, (
+        ) -> uniqueStudentList.set(ALICE, ALICE));
+    }
+
+    @Test
+    public void setStudent_editedStudentIsSameStudent_success() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.set(ALICE, ALICE);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        expectedUniqueStudentList.add(ALICE);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_editedStudentHasSameIdentity_success() {
+        uniqueStudentList.add(ALICE);
+        Student editedAlice = new StudentBuilder(ALICE).withHandle(VALID_HANDLE_BOB).withTutorials(VALID_TUTORIAL_2)
+                        .build();
+        uniqueStudentList.set(ALICE, editedAlice);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        expectedUniqueStudentList.add(editedAlice);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_editedStudentHasDifferentIdentity_success() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.set(ALICE, BOB);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        expectedUniqueStudentList.add(BOB);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudent_editedStudentHasNonUniqueIdentity_throwsDuplicateStudentException() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.add(BOB);
+        assertThrows(IllegalStateException.class, (
+        ) -> uniqueStudentList.set(ALICE, BOB));
+    }
+
+    @Test
+    public void remove_nullStudent_throwsNullPointerException() {
+        assertFalse(uniqueStudentList.remove(null));
+    }
+
+    @Test
+    public void remove_studentDoesNotExist_throwsStudentNotFoundException() {
+        assertFalse(uniqueStudentList.remove(ALICE));
+    }
+
+    @Test
+    public void remove_existingStudent_removesStudent() {
+        uniqueStudentList.add(ALICE);
+        uniqueStudentList.remove(ALICE);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudents_nullUniqueStudentList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.setAll((UniqueList<Student>) null));
+    }
+
+    @Test
+    public void setStudents_uniqueStudentList_replacesOwnListWithProvidedUniqueStudentList() {
+        uniqueStudentList.add(ALICE);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        expectedUniqueStudentList.add(BOB);
+        uniqueStudentList.setAll(expectedUniqueStudentList);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudents_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, (
+        ) -> uniqueStudentList.setAll((List<Student>) null));
+    }
+
+    @Test
+    public void setStudents_list_replacesOwnListWithProvidedList() {
+        uniqueStudentList.add(ALICE);
+        List<Student> studentList = Collections.singletonList(BOB);
+        uniqueStudentList.setAll(studentList);
+        var expectedUniqueStudentList = new UniqueList<Student>();
+        expectedUniqueStudentList.add(BOB);
+        assertEquals(expectedUniqueStudentList, uniqueStudentList);
+    }
+
+    @Test
+    public void setStudents_listWithDuplicateStudents_throwsDuplicateStudentException() {
+        List<Student> listWithDuplicateStudents = Arrays.asList(ALICE, ALICE);
+        assertThrows(IllegalStateException.class, (
+        ) -> uniqueStudentList.setAll(listWithDuplicateStudents));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, (
+        ) -> uniqueStudentList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueStudentList.asUnmodifiableObservableList().toString(), uniqueStudentList.toString());
+    }
+}
