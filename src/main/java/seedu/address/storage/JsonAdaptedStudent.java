53e736a064162078ab113dc00348796513e3cf1b
WhyAre <laiyr52@gmail.com>
2025-03-25 00:18:13 +0800

    Refactor JsonAdaptedTutorials

--- src/main/java/seedu/address/storage/JsonAdaptedStudent.java
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -7,9 +7,10 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.Phone;
@@ -21,6 +22,7 @@ import seedu.address.model.tutorial.Tutorial;
 /**
  * Jackson-friendly version of {@link Student}.
  */
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
 class JsonAdaptedStudent {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Student's %s field is missing!";
@@ -30,7 +32,7 @@ class JsonAdaptedStudent {
     private final String phone;
     private final String email;
     private final String handle;
-    private final List<JsonAdaptedTutorial> tutorials = new ArrayList<>();
+    private final List<JsonAdaptedTutorial> tutorials;
 
     /**
      * Constructs a {@code JsonAdaptedStudent} with the given student details.
@@ -46,7 +48,9 @@ class JsonAdaptedStudent {
         this.email = email;
         this.handle = handle;
         if (tutorials != null) {
-            this.tutorials.addAll(tutorials);
+            this.tutorials = new ArrayList<>(tutorials);
+        } else {
+            this.tutorials = new ArrayList<>();
         }
     }
 
@@ -59,66 +63,22 @@ class JsonAdaptedStudent {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         handle = source.getHandle().handle;
-        tutorials.addAll(source.getTutorials().stream().map(JsonAdaptedTutorial::new).collect(Collectors.toList()));
+        tutorials = source.getTutorials().stream().map(JsonAdaptedTutorial::new).toList();
     }
 
     /**
      * Converts this Jackson-friendly adapted student object into the model's
      * {@code Student} object.
-     *
-     * @throws IllegalValueException
-     *             if there were any data constraints violated in the adapted
-     *             student.
      */
-    public Student toModelType() throws IllegalValueException {
-        final List<Tutorial> studentTutorials = new ArrayList<>();
-        for (JsonAdaptedTutorial tutorial : tutorials) {
-            studentTutorials.add(tutorial.toModelType());
-        }
-
-        if (name == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
-        }
-        if (!Name.isValidName(name)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
-        }
+    public Student toModelType() {
         final Name modelName = new Name(name);
-
-        if (id == null) {
-            throw new IllegalValueException(
-                            String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentID.class.getSimpleName()));
-        }
-        if (!StudentID.isValidID(id)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
-        }
         final StudentID modelStudentId = new StudentID(id);
-
-        if (phone == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
-        }
-        if (!Phone.isValidPhone(phone)) {
-            throw new IllegalValueException(Phone.MESSAGE_CONSTRAINTS);
-        }
         final Phone modelPhone = new Phone(phone);
-
-        if (email == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
-        }
-        if (!Email.isValidEmail(email)) {
-            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
-        }
         final Email modelEmail = new Email(email);
-
-        if (handle == null) {
-            throw new IllegalValueException(
-                            String.format(MISSING_FIELD_MESSAGE_FORMAT, TelegramHandle.class.getSimpleName()));
-        }
-        if (!TelegramHandle.isValidHandle(handle)) {
-            throw new IllegalValueException(TelegramHandle.MESSAGE_CONSTRAINTS);
-        }
         final TelegramHandle modelHandle = new TelegramHandle(handle);
+        final Set<Tutorial> modelTutorials = tutorials.stream().map(JsonAdaptedTutorial::toModelType)
+                        .collect(Collectors.toCollection(HashSet::new));
 
-        final Set<Tutorial> modelTutorials = new HashSet<>(studentTutorials);
         return new Student(modelName, modelStudentId, modelPhone, modelEmail, modelHandle, modelTutorials);
     }
 
