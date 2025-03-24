6ccf34311714f0703eb3fb53d2f405ba8018bcb7
WhyAre <laiyr52@gmail.com>
2025-03-25 00:23:48 +0800

    Simplify converting to and from JSON objects (error handling is done within each class)

--- src/main/java/seedu/address/storage/JsonAdaptedTutorial.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTutorial.java
@@ -3,7 +3,6 @@ package seedu.address.storage;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.tutorial.Tutorial;
 
 /**
@@ -24,15 +23,8 @@ public class JsonAdaptedTutorial {
     /**
      * Converts this Jackson-friendly adapted tutorial object into the model's
      * {@code Tutorial} object.
-     *
-     * @throws IllegalValueException
-     *             if there were any data constraints violated in the adapted
-     *             student.
      */
-    public Tutorial toModelType() throws IllegalValueException {
-        if (!Tutorial.isValidName(name)) {
-            throw new IllegalValueException("Tutorial name is not valid.");
-        }
+    public Tutorial toModelType() {
         return new Tutorial(name);
     }
 }
