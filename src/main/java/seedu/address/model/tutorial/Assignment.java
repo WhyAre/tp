43185953e41638e1510984d1167a27fde66e86f1
WhyAre <laiyr52@gmail.com>
2025-03-25 08:53:11 +0800

    Create assignment class and jsonadaptedassignment class

---
+++ src/main/java/seedu/address/model/tutorial/Assignment.java
@@ -0,0 +1,6 @@
+package seedu.address.model.tutorial;
+
+import java.util.Date;
+
+public record Assignment(String name, Date dueDate) {
+}
---
+++ src/main/java/seedu/address/storage/JsonAdaptedAssignment.java
@@ -0,0 +1,47 @@
+package seedu.address.storage;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.Student;
+import seedu.address.model.tutorial.Assignment;
+
+/**
+ * Jackson-friendly version of {@link Student}.
+ */
+class JsonAdaptedAssignment {
+
+    private final String name;
+    private final Date dueDate;
+
+    /**
+     * Constructs a {@code JsonAdaptedStudent} with the given student details.
+     */
+    @JsonCreator
+    public JsonAdaptedAssignment(@JsonProperty("name") String name, @JsonProperty("dueDate") Date dueDate) {
+        this.name = name;
+        this.dueDate = dueDate;
+    }
+
+    /**
+     * Converts a given {@code Student} into this class for Jackson use.
+     */
+    public JsonAdaptedAssignment(Assignment source) {
+        this(source.name(), source.dueDate());
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted student object into the model's
+     * {@code Student} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted
+     *                               student.
+     */
+    public Assignment toModelType() throws IllegalValueException {
+        return new Assignment(name, dueDate);
+    }
+
+}
