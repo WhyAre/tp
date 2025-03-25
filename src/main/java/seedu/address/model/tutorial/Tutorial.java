8de0e947398af4d9ee61f188666ff366d1442c6f
WhyAre <laiyr52@gmail.com>
2025-03-25 08:53:57 +0800

    Add assignments field into Tutorial class

--- src/main/java/seedu/address/model/tutorial/Tutorial.java
+++ src/main/java/seedu/address/model/tutorial/Tutorial.java
@@ -1,11 +1,20 @@
 package seedu.address.model.tutorial;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import seedu.address.model.uniquelist.Identifiable;
 
 /**
  * Represents a tutorial
  */
-public record Tutorial(String name) implements Identifiable<Tutorial> {
+public record Tutorial(String name, List<Assignment> assignments) implements Identifiable<Tutorial> {
+
+    public Tutorial(String name) {
+        this(name, new ArrayList<>());
+    }
+
     public Tutorial {
         Objects.requireNonNull(name);
         if (!isValidName(name)) {
