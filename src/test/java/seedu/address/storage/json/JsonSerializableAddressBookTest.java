5da4fb43639409281abf0f634025e2045a193584
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-03-29 07:54:35 +0800

    Rename `DUPLICATE_STUDENT_FILE`

--- src/test/java/seedu/address/storage/json/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/json/JsonSerializableAddressBookTest.java
@@ -18,7 +18,7 @@ public class JsonSerializableAddressBookTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
     private static final Path TYPICAL_STUDENTS_FILE = TEST_DATA_FOLDER.resolve("typicalStudentsAddressBook.json");
     private static final Path INVALID_STUDENT_FILE = TEST_DATA_FOLDER.resolve("invalidStudentAddressBook.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicateStudentAddressBook.json");
+    private static final Path DUPLICATE_STUDENT_FILE = TEST_DATA_FOLDER.resolve("duplicateStudentAddressBook.json");
 
     @Test
     public void toModelType_typicalStudentsFile_success() throws Exception {
@@ -39,7 +39,7 @@ public class JsonSerializableAddressBookTest {
     @Test
     public void toModelType_duplicateStudents_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil
-                        .readJsonFile(DUPLICATE_PERSON_FILE, JsonSerializableAddressBook.class).get();
+                        .readJsonFile(DUPLICATE_STUDENT_FILE, JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
                         dataFromFile::toModelType);
     }
