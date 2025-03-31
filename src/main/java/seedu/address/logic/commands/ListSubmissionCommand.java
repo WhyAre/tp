b1fa74abcc124f5dead5da8d023b115e65f04f34
WhyAre <laiyr52@gmail.com>
2025-03-31 22:22:19 +0800

    Support filtering in the list command

--- src/main/java/seedu/address/logic/commands/ListSubmissionCommand.java
+++ src/main/java/seedu/address/logic/commands/ListSubmissionCommand.java
@@ -15,16 +15,26 @@ public class ListSubmissionCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Successfully listed submissions";
 
+    private final String tutorialName;
+    private final String studentName;
+    private final String assignmentName;
+
     /**
      * Creates a {@link SetSubmissionCommand} object
      */
-    public ListSubmissionCommand() {
-
+    public ListSubmissionCommand(String tutorialName, String studentName, String assignmentName) {
+        this.tutorialName = tutorialName;
+        this.studentName = studentName;
+        this.assignmentName = assignmentName;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        model.updateFilteredSubmissionList(s -> s.assignment().name().startsWith(assignmentName)
+                        && s.assignment().tutorial().name().startsWith(tutorialName)
+                        && s.student().getName().toString().startsWith(studentName));
 
         return new CommandResult(MESSAGE_SUCCESS, NavigationMode.SUBMISSION);
     }
--- src/main/java/seedu/address/logic/parser/ListSubmissionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListSubmissionCommandParser.java
@@ -3,12 +3,24 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.ListSubmissionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_NAME;
+
 /**
  * Parser for submission list command
  */
 public class ListSubmissionCommandParser implements Parser<ListSubmissionCommand> {
     @Override
     public ListSubmissionCommand parse(String userInput) throws ParseException {
-        return new ListSubmissionCommand();
+        // Workaround, a space must be prepended if not it can't detect the flag nicely
+        // Will have to look into tokenize method to fix it
+        var multimap = ArgumentTokenizer.tokenize(" " + userInput, PREFIX_STUDENT_NAME, PREFIX_ASSIGNMENT_NAME, PREFIX_TUTORIAL_NAME);
+
+        var tutorialName = multimap.getValue(PREFIX_TUTORIAL_NAME).orElse("");
+        var studentName = multimap.getValue(PREFIX_STUDENT_NAME).orElse("");
+        var assignmentName = multimap.getValue(PREFIX_ASSIGNMENT_NAME).orElse("");
+
+        return new ListSubmissionCommand(tutorialName, studentName, assignmentName);
     }
 }
