f5071ade90c23ab97393799727b6cf3de8e1b52e
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-04-02 02:01:55 +0800

    Update command usage messages

--- src/main/java/seedu/address/logic/parser/SubmissionParser.java
+++ src/main/java/seedu/address/logic/parser/SubmissionParser.java
@@ -28,10 +28,7 @@ public class SubmissionParser implements Parser<Command> {
 
         usage = """
                         Usage: submission COMMAND
-
-                        COMMAND:
-                        %s""".formatted(
-                        subcmds.keySet().stream().map("  - %s"::formatted).collect(Collectors.joining("\n")));
+                        COMMAND: %s""".formatted(String.join(", ", subcmds.keySet()));
     }
 
     @Override
