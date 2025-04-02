d70ebee5c9bb735d4dbf8cacd9f283ca4eab1574
WhyAre <laiyr52@gmail.com>
2025-04-03 00:24:05 +0800

    Add checks for nav mode

--- src/main/java/seedu/address/logic/commands/DeleteAssignmentCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAssignmentCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INCORRECT_NAVIGATION_MODE;
 import static seedu.address.logic.Messages.MESSAGE_TUTORIAL_NOT_FOUND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_IDX;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.NavigationMode;
@@ -44,6 +46,10 @@ public class DeleteAssignmentCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        if (model.getNavigationMode() != NavigationMode.TUTORIAL) {
+            throw new CommandException(MESSAGE_INCORRECT_NAVIGATION_MODE.formatted(NavigationMode.TUTORIAL));
+        }
 
         for (Index idx : indices) {
             int idxZeroBased = idx.getZeroBased();
